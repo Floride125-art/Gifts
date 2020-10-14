deda1a1987e1aaa3d126fdd482cad377d35337e4
Aimee-pacy <ishimwefique@gmail.com>
2020-10-14 20:12:37 +0200

    Added function for contact us and submit btn

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#give").click(function(){
+        var inputName = $("input#inputName").val();
+        alert(inputName + " we have received your message. Thank you for reaching out to us.");
+       
+  
+    });
+  
+  });
